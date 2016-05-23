75aa5d11ea4ff40d74c4e6777bc7f10d36f314af
Biz Cook <elizabeth.myles.cook@gmail.com>
2016-05-23 10:50:25 -0500

    test

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+  <title>temp for Biz</title>
+  </head>
+  <body>
+  <p>You can put HTML right in your PHP:</p>
+  <p>Adding two numbers in PHP here:
+  <?php
+    $number = 3 * 9;
+    echo $number;
+  ?>
+  </p>
+  <p>
+     ... and you can include other PHP as "partials":
+     <?php include 'other.php';?>
+  </p>
+  <h2>I know you'll be able to figure this stuff out EASILY</h2>
+  </body>
+</html>
---
+++ other.php
@@ -0,0 +1,7 @@
+<p>
+  from other.php... some more math!
+</p>
+<?php
+  $someOtherNumber = 9 * 9;
+  echo $someOtherNumber;
+?>
