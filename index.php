d91f8efdbfb045bff2f64a025c7ce85b4e4527ba
Biz Cook <elizabeth.myles.cook@gmail.com>
2016-05-25 16:02:49 -0500

    added some variables and if/else statements

--- index.php
+++ index.php
@@ -9,12 +9,20 @@
     $number = 3 * 9;
     echo $number;
   ?>
+  <h2>
+  <?php
+    if(5>6){
+      echo "this ain't right!";
+    }else{
+      echo "you r a smartypants!";
+    }
+    ?></h2>
   </p>
   <p>
      ... and you can include other PHP as "partials":
      <?php include 'other.php';?>
   </p>
-  <h2>I know you'll be able to figure this stuff out EASILY</h2>
+  <h2>below is a footer partial!!</h2>
   <?php include 'footer.php';?>
   </body>
 </html>
